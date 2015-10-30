0c097f232c1b6522b94998e6b45d744224a65024
Oğuzhan Soykan <oguzhansoykan@gmail.com>
2015-10-30 17:18:03 +0300

    saga initial

---
+++ src/Dynamic.Translator.Saga/Translator.cs
@@ -0,0 +1,14 @@
+﻿namespace Dynamic.Translator.Saga
+{
+    #region
+
+    using System;
+    using Automatonymous;
+
+    #endregion
+
+    public class Translator : SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get; set; }
+    }
+}
