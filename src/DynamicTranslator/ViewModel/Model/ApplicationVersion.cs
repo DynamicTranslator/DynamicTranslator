05786fb75406c52222090324c82b8cc9f816e9a4
Oğuzhan Soykan <oguzhansoykan@gmail.com>
2015-12-16 00:13:28 +0200

    increment version

--- src/DynamicTranslator/ViewModel/Model/ApplicationVersion.cs
+++ src/DynamicTranslator/ViewModel/Model/ApplicationVersion.cs
@@ -5,10 +5,11 @@
         public const string Version100 = "1.0.0";
         public const string Version200 = "2.0.0";
         public const string Version310 = "3.1.0";
+        public const string Version320 = "3.2.0";
 
         public static string GetCurrentVersion()
         {
-            return Version310;
+            return Version320;
         }
     }
 }
