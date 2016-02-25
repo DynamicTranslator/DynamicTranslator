a6e9a48a578ff0c53349ed6658912e82e2cc9ce0
Oğuzhan Soykan <oguzhansoykan@gmail.com>
2016-02-25 15:47:23 +0200

    Unit tests csproj added

---
+++ src/DynamicTranslator.Test/Finder/GoogleTranslateFinderTest.cs
@@ -0,0 +1,43 @@
+﻿#region using
+
+using System.Collections.Generic;
+using DynamicTranslator.Core.Config;
+using DynamicTranslator.Core.Orchestrators.Finder;
+using DynamicTranslator.Core.Orchestrators.Model;
+using DynamicTranslator.Core.ViewModel.Constants;
+using DynamicTranslator.Test.Helper;
+using FluentAssertions;
+using Moq;
+using Ploeh.AutoFixture.Xunit;
+using Xunit;
+
+#endregion
+
+namespace DynamicTranslator.Test.Finder
+{
+    public class GoogleTranslateFinderTest
+    {
+        [Theory, AutoDataMoq]
+        public void Find_And_ThrowNothing(
+            [Frozen] Mock<IMeanFinderFactory> finderFactoryMock,
+            [Frozen] Mock<IMeanFinder> googleTranslateFinderMock,
+            [Frozen] Mock<IStartupConfiguration> startupConfigurationMock,
+            [Frozen] Mock<TranslateRequest> requestMock)
+        {
+            googleTranslateFinderMock.Setup(x => x.TranslatorType).Returns(TranslatorType.GOOGLE);
+            finderFactoryMock.Setup(x => x.GetFinders())
+                             .Returns(new List<IMeanFinder> {googleTranslateFinderMock.Object});
+
+            requestMock.Setup(x => x.CurrentText).Returns("Hi");
+            requestMock.Setup(x => x.FromLanguageExtension).Returns("en");
+
+            startupConfigurationMock.Setup(x => x.ToLanguageExtension).Returns("tr");
+
+            var sut = googleTranslateFinderMock.Object;
+
+            sut.Invoking(async x => await x.Find(requestMock.Object))
+               .Should()
+               .Be(new TranslateResult(true, new Maybe<string>("selam")));
+        }
+    }
+}
---
+++ src/DynamicTranslator.Test/Helper/AutoDataMoq.cs
@@ -0,0 +1,27 @@
+﻿#region using
+
+using System;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.Xunit;
+
+#endregion
+
+namespace DynamicTranslator.Test.Helper
+{
+    public class AutoDataMoq : AutoDataAttribute
+    {
+        public AutoDataMoq()
+            : base(new Fixture().Customize(new AutoMoqCustomization()))
+        {
+        }
+
+        public AutoDataMoq(Type type, params object[] parameters)
+            : this()
+        {
+            object obj = Activator.CreateInstance(type, parameters);
+
+            base.Fixture.Customize(obj as ICustomization);
+        }
+    }
+}
