ce6db5e26601554443c29153c5bd9caa135a357a
Ç. Çavuşoğlu <cavusoglucagatay@gmail.com>
2016-06-02 15:31:38 +0300

    initial unit tests

---
+++ src/DynamicTranslator.Tests/UnitOfWorkBaseTest.cs
@@ -0,0 +1,23 @@
+﻿using DynamicTranslator.Core.DBReezeNoSQL.Uow;
+
+namespace DynamicTranslator.Tests
+{
+    using System;
+    using DynamicTranslator.Core.Domain.Uow;
+    using Xunit;
+
+    public class UnitOfWorkBaseTest
+    {
+        [Fact]
+        public void Begin_IfOptionsIsNull_ThrowsArgumentNullException()
+        {
+            #region Arrange
+            UnitOfWorkBase sut = new DbReezeUnitOfWork(null, null);
+            #endregion
+
+            #region Act & Assert
+            Assert.Throws(typeof(ArgumentNullException), () => sut.Begin(null));
+            #endregion
+        }
+    }
+}
