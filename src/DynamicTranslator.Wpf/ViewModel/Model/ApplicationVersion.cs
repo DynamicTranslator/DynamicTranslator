97565776cf060e4f234b3cb5d6b15c34db787145
Oğuzhan Soykan <oguzhansoykan@gmail.com>
2016-06-26 17:13:01 +0300

    version incremented

--- src/DynamicTranslator.Wpf/ViewModel/Model/ApplicationVersion.cs
+++ src/DynamicTranslator.Wpf/ViewModel/Model/ApplicationVersion.cs
@@ -2,7 +2,7 @@
 {
     public static class ApplicationVersion
     {
-        public const string CurrentVersion = "3.2.1";
+        public const string CurrentVersion = "3.3.1";
 
         public static string GetCurrentVersion()
         {
