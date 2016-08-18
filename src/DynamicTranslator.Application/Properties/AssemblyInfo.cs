2c7195991d4e267b1817d75030ff98aaa0ed895c
Oğuzhan Soykan <oguzhansoykan@gmail.com>
2016-08-18 23:13:14 +0300

    version incremented to 3.4.1

--- src/DynamicTranslator.Application/Properties/AssemblyInfo.cs
+++ src/DynamicTranslator.Application/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
+using DynamicTranslator;
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -35,5 +37,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyVersion(ApplicationVersion.CurrentVersion)]
+[assembly: AssemblyFileVersion(ApplicationVersion.CurrentVersion)]
--- src/DynamicTranslator.Wpf/Properties/AssemblyInfo.cs
+++ src/DynamicTranslator.Wpf/Properties/AssemblyInfo.cs
@@ -10,6 +10,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
+using DynamicTranslator;
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -19,7 +21,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MIT")]
 [assembly: AssemblyProduct("DynamicTranslator.Wpf")]
-[assembly: AssemblyCopyright("Copyright ©  2012 Oğuzhan Soykan")]
+[assembly: AssemblyCopyright("Copyright © 2016 Oğuzhan Soykan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -55,5 +57,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(ApplicationVersion.CurrentVersion)]
+[assembly: AssemblyFileVersion(ApplicationVersion.CurrentVersion)]
