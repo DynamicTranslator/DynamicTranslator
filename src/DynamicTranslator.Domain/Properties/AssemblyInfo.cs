8406e890754ee660a1b413030057f120686000c0
Oguzhan Soykan <o.soykan@evision-software.com>
2018-09-28 09:32:46 +0200

    new csproj type

--- src/DynamicTranslator.Domain/Properties/AssemblyInfo.cs
+++ src/DynamicTranslator.Domain/Properties/AssemblyInfo.cs
@@ -3,19 +3,6 @@ using System.Runtime.InteropServices;
 
 using DynamicTranslator;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("DynamicTranslator.Domain")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DynamicTranslator.Domain")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -25,17 +12,3 @@ using DynamicTranslator;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("9205050a-26a4-4047-a691-d6524eef8f1c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion(ApplicationVersion.CurrentVersion)]
-[assembly: AssemblyFileVersion(ApplicationVersion.CurrentVersion)]
